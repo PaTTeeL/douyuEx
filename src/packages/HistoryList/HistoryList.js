78ce44a2096f8b6409cd531f912b1fe91e0239df
PaTTeeL <PaTTeeL@Hotmail.com>
2026-01-08 05:18:09 +0800

    feat: 限制HistoryList的最大高度并启用溢出滚动

--- src/packages/HistoryList/HistoryList.js
+++ src/packages/HistoryList/HistoryList.js
@@ -5,8 +5,8 @@ function initPkg_HistoryList() {
             if (typeof url === 'string' && /\/(japi\/watchHistory\/apinc\/getHistoryList|wgapi\/vodnc\/front\/live\/history\/getList)/.test(url)) {
                 //console.log('DouyuEx 历史列表: 发现历史列表请求，修改URL...');
                 try {
-                    url = url.replace(/([?&](?:num|limit))=\d+/, '$1=8');
-                    //console.log('DouyuEx 历史列表: 已将历史列表请求数量改为 8');
+                    url = url.replace(/([?&](?:num|limit))=\d+/, '$1=99');
+                    //console.log('DouyuEx 历史列表: 已将历史列表请求数量改为 99');
                 } catch (e) {
                     console.error('DouyuEx 历史列表: 修改历史列表请求数量失败', e);
                 }
@@ -14,4 +14,18 @@ function initPkg_HistoryList() {
             return originalXHROpen.call(this, method, url, ...args);
         };
     })();
+
+    gDomObserver.waitForElement('.Header-history-content').then(historyContent => {
+        new ResizeObserver((entries, observerInstance) => {
+            const dropPane = entries[0].target.querySelector('.DropPane-drop');
+            if (!dropPane || dropPane.childElementCount === 0) return;
+            observerInstance.disconnect();
+            const dropPaneRect = dropPane.getBoundingClientRect();
+            const dropMenuRect = dropPane.closest('.public-DropMenu-drop').getBoundingClientRect();
+            const spaceAbove = dropPaneRect.top;
+            const spaceBelow = dropMenuRect.bottom - dropPaneRect.bottom;
+            const extraOffset = (document.documentElement.scrollWidth > document.documentElement.clientWidth) ? 15 : 9;
+            dropPane.style.setProperty('--historylist-max-height', `calc(100dvh - ${spaceAbove}px - ${spaceBelow}px - ${extraOffset}px)`);
+        }).observe(historyContent);
+    });
 }
