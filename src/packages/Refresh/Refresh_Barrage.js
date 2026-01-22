a2dbb3264a3ae89e06e3e38c8aa607e916d5c659
PaTTeeL <PaTTeeL@Hotmail.com>
2026-01-22 19:41:06 +0800

    fix: 合并Refresh_BarrageFrame到Refresh_Barrage

--- src/packages/Refresh/Refresh_Barrage.js
+++ src/packages/Refresh/Refresh_Barrage.js
@@ -14,12 +14,38 @@ function Refresh_Barrage_insertIcon() {
     a.className = "refresh-barrage";
     a.id = "refresh-barrage";
 	a.innerHTML = '<svg t="1588051109604" id="refresh-barrage__svg" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3095" width="16" height="16"><path d="M588.416 516.096L787.2 317.312a54.016 54.016 0 1 0-76.416-76.416L512 439.68 313.216 241.024A54.016 54.016 0 1 0 236.8 317.376l198.784 198.848-198.016 197.888a54.016 54.016 0 1 0 76.416 76.416L512 592.576l197.888 197.952a54.016 54.016 0 1 0 76.416-76.416L588.416 516.096z" fill="#AFAFAF" p-id="3096"></path></svg><i class="Barrage-toolbarIcon"></i><span id="refresh-barrage__text" class="Barrage-toolbarText">前缀</span>';
-	let b = document.getElementsByClassName("Barrage-toolbar")[0];
-	b.insertBefore(a, b.childNodes[0]);
+
+	let b = document.createElement("a");
+    b.className = "Barrage-toolbarLock";
+    b.id = "refresh-barrage-frame";
+	b.innerHTML = '<i class="Barrage-toolbarIcon"></i><span id="refresh-barrage-frame__text" class="Barrage-toolbarText">拉高</span>';
+	let c = document.getElementsByClassName("Barrage-toolbar")[0];
+	c.prepend(a, b);
 }
 
 function initPkg_Refresh_Barrage_Func() {
-	document.getElementById("refresh-barrage").addEventListener("click", function() {
+	document.getElementById("refresh-barrage-frame").addEventListener("click", function() {
+        let dom_rank = document.getElementsByClassName("layout-Player-rank")[0];
+        let dom_activity = document.getElementById("js-room-activity");
+        let dom_topBarrage = document.getElementsByClassName("Barrage")[0];
+        if (dom_rank.style.display == "none") {
+            // 被拉高
+            dom_rank.style.display = "block";
+            dom_activity.style.display = "block";
+            dom_topBarrage.className = "Barrage";
+            document.getElementById("refresh-barrage-frame__text").innerText = "拉高";
+
+        } else {
+            // 没拉高
+            dom_rank.style.display = "none";
+            dom_activity.style.display = "none";
+            dom_topBarrage.className = "Barrage top-0-important";
+            document.getElementById("refresh-barrage-frame__text").innerText = "恢复";
+        }
+        saveData_Refresh();
+    });
+
+    document.getElementById("refresh-barrage").addEventListener("click", function() {
         if (current_barrage_status == 0) {
             // 简化
             setRefreshBarrage();
@@ -30,6 +56,16 @@ function initPkg_Refresh_Barrage_Func() {
     });
 }
 
+function refresh_BarrageFrame_getStatus() {
+    let dom_rank = document.getElementsByClassName("layout-Player-rank")[0];
+    if (dom_rank.style.display == "none") {
+        // 被拉高
+        return true;
+    } else {
+        // 没拉高
+        return false;
+    }
+}
 
 function refresh_Barrage_getStatus() {
     if (current_barrage_status == 1) {
@@ -45,6 +81,16 @@ function initPkg_Refresh_Barrage_Set() {
     let ret = localStorage.getItem("ExSave_Refresh");
     if (ret != null) {
         let retJson = JSON.parse(ret);
+        if ("barrageFrame" in retJson == false) {
+            retJson.barrageFrame = {status: false};
+        }
+        if (retJson.barrageFrame.status == true) {
+            let dom_rank = document.getElementsByClassName("layout-Player-rank")[0];
+            let dom_activity = document.getElementById("js-room-activity");
+            dom_rank.style.display = "none";
+            dom_activity.style.display = "none";
+            document.getElementById("refresh-barrage-frame__text").innerText = "恢复";
+        }
         if ("barrage" in retJson == false) {
             retJson.barrage = {status: false};
         }
