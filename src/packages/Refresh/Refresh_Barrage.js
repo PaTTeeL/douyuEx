d0bc45b02d502384243fdbfb5f1740ec612ee5d2
PaTTeeL <PaTTeeL@Hotmail.com>
2026-01-23 04:05:00 +0800

    feat: 使用CSS状态切换模式重构Refresh_Barrage

--- src/packages/Refresh/Refresh_Barrage.js
+++ src/packages/Refresh/Refresh_Barrage.js
@@ -1,110 +1,57 @@
-let current_barrage_status = 0; // 0没被简化 1被简化
-
 function initPkg_Refresh_Barrage() {
-    Promise.all([
-        gDomObserver.waitForElement('.Barrage-toolbar'),
-        gDomObserver.waitForElement('.layout-Player-rank')
-    ]).then(([toolbar, dom_rank]) => {
+    gDomObserver.waitForElement('.Barrage-toolbar').then(toolbar => {
         initPkg_Refresh_Barrage_Dom(toolbar);
-        initPkg_Refresh_Barrage_Func(toolbar, dom_rank);
+        initPkg_Refresh_Barrage_Func(toolbar);
         initPkg_Refresh_Barrage_Set();
     });
 }
 
 function initPkg_Refresh_Barrage_Dom(toolbar) {
-    if (!toolbar.querySelector(".refresh-barrage")) {
+    if (!toolbar.querySelector(".Barrage-simpleMode")) {
         toolbar.insertAdjacentHTML(
             "afterbegin",
-            `<a class="refresh-barrage" id="refresh-barrage">
-                <svg t="1588051109604" id="refresh-barrage__svg" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3095" width="16" height="16"><path d="M588.416 516.096L787.2 317.312a54.016 54.016 0 1 0-76.416-76.416L512 439.68 313.216 241.024A54.016 54.016 0 1 0 236.8 317.376l198.784 198.848-198.016 197.888a54.016 54.016 0 1 0 76.416 76.416L512 592.576l197.888 197.952a54.016 54.016 0 1 0 76.416-76.416L588.416 516.096z" fill="#AFAFAF" p-id="3096"></path></svg><i class="Barrage-toolbarIcon"></i><span id="refresh-barrage__text" class="Barrage-toolbarText">前缀</span>
+            `<a class="Barrage-simpleMode" id="btn-simpleBarrage">
+                <svg t="1588051109604" id="btn-simpleBarrage__svg" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3095" width="16" height="16"><path d="M588.416 516.096L787.2 317.312a54.016 54.016 0 1 0-76.416-76.416L512 439.68 313.216 241.024A54.016 54.016 0 1 0 236.8 317.376l198.784 198.848-198.016 197.888a54.016 54.016 0 1 0 76.416 76.416L512 592.576l197.888 197.952a54.016 54.016 0 1 0 76.416-76.416L588.416 516.096z" fill="#AFAFAF" p-id="3096"></path></svg>
+                <i class="Barrage-toolbarIcon"></i>
+                <span id="btn-simpleBarrage__text" class="Barrage-toolbarText"></span>
             </a>
-            <a class="Barrage-toolbarLock" id="refresh-barrage-frame">
-                <i class="Barrage-toolbarIcon"></i><span id="refresh-barrage-frame__text" class="Barrage-toolbarText">拉高</span>
+            <a class="Barrage-toolbarLock" id="btn-rankHidden">
+                <i class="Barrage-toolbarIcon"></i>
+                <span id="btn-rankHidden__text" class="Barrage-toolbarText"></span>
             </a>`
         );
     }
 }
 
-function initPkg_Refresh_Barrage_Func(toolbar, dom_rank) {
+function initPkg_Refresh_Barrage_Func(toolbar) {
     toolbar.addEventListener("click", e => {
-        if (e.target.closest("#refresh-barrage-frame")) {
-            let dom_activity = document.getElementById("js-room-activity");
-            let dom_topBarrage = toolbar.closest('.Barrage');
-            if (dom_rank.style.display == "none") {
-                // 被拉高
-                dom_rank.style.display = "block";
-                dom_activity.style.display = "block";
-                dom_topBarrage.className = "Barrage";
-                document.getElementById("refresh-barrage-frame__text").innerText = "拉高";
-
-            } else {
-                // 没拉高
-                dom_rank.style.display = "none";
-                dom_activity.style.display = "none";
-                dom_topBarrage.className = "Barrage top-0-important";
-                document.getElementById("refresh-barrage-frame__text").innerText = "恢复";
-            }
+        if (e.target.closest("#btn-simpleBarrage")) {
+            document.body.classList.toggle("is-barrageSimple");
             saveData_Refresh();
-        } else if (e.target.closest("#refresh-barrage")) {
-            if (current_barrage_status == 0) {
-                // 简化
-                setRefreshBarrage();
-            } else {
-                cancelRefreshBarrage();
-            }
+        } else if (e.target.closest("#btn-rankHidden")) {
+            document.body.classList.toggle("is-rankHidden");
             saveData_Refresh();
         }
     });
 }
 
 function refresh_BarrageFrame_getStatus() {
-    let dom_rank = document.getElementsByClassName("layout-Player-rank")[0];
-    return dom_rank ? dom_rank.style.display == "none" : false;
+    return document.body.classList.contains("is-rankHidden");
 }
 
 function refresh_Barrage_getStatus() {
-    return current_barrage_status == 1 ? true : false;
+    return document.body.classList.contains("is-barrageSimple");
 }
 
 function initPkg_Refresh_Barrage_Set() {
     let ret = localStorage.getItem("ExSave_Refresh");
     if (ret != null) {
         let retJson = JSON.parse(ret);
-        if ("barrageFrame" in retJson == false) {
-            retJson.barrageFrame = {status: false};
+        if (retJson.barrageFrame && retJson.barrageFrame.status === true) {
+            document.body.classList.add("is-rankHidden");
         }
-        if (retJson.barrageFrame.status == true) {
-            let dom_rank = document.getElementsByClassName("layout-Player-rank")[0];
-            let dom_activity = document.getElementById("js-room-activity");
-            dom_rank.style.display = "none";
-            dom_activity.style.display = "none";
-            document.getElementById("refresh-barrage-frame__text").innerText = "恢复";
-        }
-        if ("barrage" in retJson == false) {
-            retJson.barrage = {status: false};
-        }
-        if (retJson.barrage.status == true) {
-            setRefreshBarrage();
+        if (retJson.barrage && retJson.barrage.status === true) {
+            document.body.classList.add("is-barrageSimple");
         }
     }
-}
- 
-function setRefreshBarrage() {
-    let cssText = `
-    .UserCsgoGameDataMedal,.Barrage-honor,.Barrage-listItem .Barrage-icon,.Barrage-listItem .FansMedal.is-made,.Barrage-listItem .RoomLevel,.Barrage-listItem .Motor,.Barrage-listItem .ChatAchievement,.Barrage-listItem .Barrage-hiIcon,.Barrage-listItem .Medal,.Barrage-listItem .MatchSystemTeamMedal{display:none !important;}
-    /*.Barrage-listItem .UserLevel{display:none !important;}*/
-    .Barrage-listItem .Baby{display:none !important;}
-    .FansMedalWrap{display:none !important;}
-    `;
-    StyleHook_set("Ex_Style_RefreshBarrage", cssText);
-    current_barrage_status = 1;
-    document.getElementById("refresh-barrage").style.backgroundColor = "rgb(18,150,219)";
-    document.getElementById("refresh-barrage__text").style.color = "#fff";
-}
-
-function cancelRefreshBarrage() {
-    StyleHook_remove("Ex_Style_RefreshBarrage");
-    current_barrage_status = 0;
-    document.getElementById("refresh-barrage").style.backgroundColor = "";
-    document.getElementById("refresh-barrage__text").style.color = "";
 }
